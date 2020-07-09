eb31647f62ca895fbda47145c406a04b739e6356
Emerson Lisboa - 475 <emerson.lisboa@Bx.PCEBG.COM>
2020-07-09 11:06:27 -0300

    Ajuste tela

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
       <div>
         <nav className="navbar navbar-expand navbar-dark bg-dark">
           <a href="/grade" className="navbar-brand">
-            Aplicativo
+            Aplicativo 1.0
           </a>
           <div className="navbar-nav mr-auto">
             <li className="nav-item">
